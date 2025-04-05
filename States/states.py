5421664ca77be8ba62c5b8aecd9b220f5341951e
mello <elijahcook1201@gmail.com>
2025-04-05 16:44:00 -0600

    Rename to follow convention

---
+++ States/states.py
@@ -0,0 +1,48 @@
+import math
+
+from pygame.sprite import AbstractGroup
+from utility import *
+from Entities import *
+from gameMap import *
+
+
+class State:
+    '''Abstract class for game states'''
+    def __init__(self) -> None:
+        pass
+    
+    def process(self, events):
+        pass
+
+    def update(self):
+        pass
+
+    def render(self):
+        pass
+
+
+class MultiFrameAction:
+    def __init__(self) -> None:
+        self.completed = False
+    
+    def update(self, deltaTime: int):
+        print("update method for multiframeAction not implemented")
+
+class StartMenu(State):
+    def __init__(self, game, playState) -> None:
+        self.game = game
+        self.playButton = TextImg("Play")
+        self.playButton.rect.center = game.screen.get_rect().center
+        self.playState = playState
+
+    def process(self, events):
+        super().process(events)
+        for event in events:
+            if event.type == pg.MOUSEBUTTONDOWN and event.button == pg.BUTTON_LEFT:
+                if self.playButton.rect.collidepoint(event.pos):
+                    self.game.enterState(self.playState)
+                    print("go to next state")
+
+    def render(self):
+        self.game.screen.fill("black")
+        self.game.screen.blit(self.playButton.image, self.playButton.rect)
